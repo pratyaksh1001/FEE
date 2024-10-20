81ec2171c5469f3ac42b42ba3b6b77a5bca58b88
pratyksh1001 <pratyakshkarmahe@gmail.com>
2024-10-20 11:43:07 +0530

    nothing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import react from "react"
 import reactdom from "react-dom"
-
-
-function Card(param){
-    return(
-        <div>
-            <p>{param.name}</p>
-        </div>
-    )
-}
-
-reactdom.render(<Card name={"pratyaksh"}/>,document.getElementById("root"))
